91b2ccdd4c5fb2df29647de21356032f53af7c12
Maurice <maurice.nzai@gmail.com>
2024-08-09 08:40:19 +0000

    Add Funtion to Sort Array Using Seletion Sort
    Added function 'selection_sort' to sort a given list of numbers in ascending order
    Implemented by using nested for loops

---
+++ day_31/2-selection_sort.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Perform selection sort on the given list of numbers.
+
+    Args:
+        nums (list of int): The list of integers to be sorted.
+
+    Returns:
+        list of int: The sorted list in ascending order.
+
+    Example:
+        >>> selection_sort([64, 25, 12, 22, 11])
+        [11, 12, 22, 25, 64]
+"""
+
+
+def selection_sort(nums):
+    """
+    Perform selection sort on the given list of numbers.
+
+    Args:
+        nums (list of int): The list of integers to be sorted.
+
+    Returns:
+        list of int: The sorted list in ascending order.
+
+    Example:
+        >>> selection_sort([64, 25, 12, 22, 11])
+        [11, 12, 22, 25, 64]
+    """
+    n = len(nums)
+
+    #
+    for i in range(n - 1):
+        smallest = i
+
+        # Find index of the smallest element in the remaining unsorted array
+        for j in range(i + 1, n):
+            if nums[j] < nums[smallest]:
+                smallest = j
+        
+        # Swap the found smallest element with the first element.
+        if i != smallest:
+            nums[i], nums[smallest] = nums[smallest], nums[i]
+
+    return nums
+
+
+# Example usage:
+if __name__ == "__main__":
+    array = [64, 25, 12, 22, 11]
+    sorted_array = selection_sort(array)
+    print(sorted_array)  # Output: [11, 12, 22, 25, 64]
